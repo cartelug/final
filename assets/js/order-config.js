ea37b1034b34ce87696bc6b1bd684bce5e07ec38
Neeza Amani Shyaka <shyakaneeza@gmail.com>
2026-07-29 10:14:18 +0300

    Build 97 World storefront

---
+++ assets/js/order-config.js
@@ -0,0 +1,12 @@
+window.ORDER_CONFIG = {
+  supportWhatsApp: '256762193386',
+  sheetEndpoint: 'https://script.google.com/macros/s/AKfycbzsER7toUR8OwPWPic7Oqbbjz-ew2pR_HJ4Um3V9o6eVmlf730ibwF7ELv6GCekmgl2aA/exec',
+  termsVersion: '97-world-v2',
+  products: {
+    prime: { offer: 'Prime Video', source: 'prime-order' },
+    appleTv: { offer: 'Apple TV+ interest request', source: 'appletv-order' },
+    growth: { offer: '97 World Social Growth', source: '97-growth' },
+    digital: { offer: '97 World Business Website Package', source: '97-digital' },
+    enterprise: { offer: '97 World Business Systems discovery', source: '97-enterprise' }
+  }
+};
